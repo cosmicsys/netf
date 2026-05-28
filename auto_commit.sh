33f580a334bfcccc1f5348be8eb735096644d1c2
cosmicsys <cosmicsys@users.noreply.github.com>
2026-05-28 16:54:11 +0200

    auto sync 1779980051

--- auto_commit.sh
+++ auto_commit.sh
@@ -1,19 +1,21 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+BRANCH="main"
+
 if ! git rev-parse --is-inside-work-tree >/dev/null 2>&1; then
   echo "Not git repo"
   exit 1
 fi
-
-echo "Watching repo:"
-git remote get-url origin
 
 while true; do
   git add -A
 
   if ! git diff --cached --quiet; then
     git commit -m "auto sync $(date +%s)"
-    git push
+
+    git pull --rebase origin $BRANCH
+
+    git push origin $BRANCH
   fi
 
   sleep 3
